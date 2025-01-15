d70bebf0df78a4bde69ed71093d83a4471b12b6c
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-01-15 04:33:39 +0100

    Test classes in console

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import "./css/normalize.css";
 import "./css/style.css";
+import { Todo, Todos } from "./Todo.js";
+const allTodos = new Todos();
+allTodos.add(new Todo("fix machine", "manual", "2025-01-15", 4));
+allTodos.add(new Todo("gym", "personal", "2024-12-15", 2));
+allTodos.add(new Todo("read book", "personal", "2023-01-15", 4));
+allTodos.add(new Todo("apply for job", "manual", "2024-11-07", 4));
+allTodos.add(new Todo("fix table", "manual", "2025-01-30", 4));
+
+allTodos.sortByDate().forEach((todo) => console.log(todo.toString()));
