80689639a75cc102248ef6d2511dbdc107f68395
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-01-16 06:05:06 +0100

    Dialog handler class

---
+++ src/Dialog.js
@@ -0,0 +1,45 @@
+import { format } from "date-fns";
+export default class Dialog {
+  #dialog;
+  #openBtn;
+  #exitBtn;
+  #form;
+  constructor() {
+    this.#dialog = document.querySelector("#dialog");
+    let dateStr = format(new Date(), "yyyy-MM-dd");
+    const date = document.querySelector("#date");
+    date.setAttribute("min", dateStr);
+    date.setAttribute("value", dateStr);
+    this.#openBtn = document.querySelector("#add_todo");
+    this.#exitBtn = document.querySelector("#exit_dialog");
+    this.#form = document.querySelector("#form");
+    this.#eventListeners();
+  }
+
+  #eventListeners() {
+    this.#openBtn.addEventListener("click", () => this.#open());
+    this.#exitBtn.addEventListener("click", () => this.#close());
+    this.#dialog.addEventListener("click", (e) => this.#closeOnOutsideClick(e));
+  }
+
+  #open() {
+    this.#dialog.showModal();
+  }
+
+  #close() {
+    this.#dialog.close();
+    this.#form.reset();
+  }
+
+  #closeOnOutsideClick(e) {
+    const rect = this.#dialog.getBoundingClientRect();
+    if (
+      e.clientX < rect.left ||
+      e.clientX > rect.right ||
+      e.clientY < rect.top ||
+      e.clientY > rect.bottom
+    ) {
+      this.#close();
+    }
+  }
+}
