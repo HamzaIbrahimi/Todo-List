6de72ca981e042b552f4f3974c8f15fa407c96c8
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-01-17 05:11:07 +0100

    Configure for mp3 files

--- webpack.common.js
+++ webpack.common.js
@@ -38,6 +38,10 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: "asset/resource",
       },
+      {
+        test: /\.mp3$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };
