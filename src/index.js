015138d7dcb236b7a75d3faf8685e6447beaa90a
HamzaIbrahimi <132024580+HamzaIbrahimi@users.noreply.github.com>
2025-01-18 22:01:14 +0100

    Removal todo on click and move sound to the remove event listener

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./css/normalize.css";
 import "./css/style.css";
-import pop from "../sound/pop-94319.mp3";
+import pop from "../sound/pop-268648.mp3";
+import snackbar from "snackbar";
 import { Todos } from "./Todo.js";
 import { createTodoFromForm, renderTodo } from "./createTodo.js";
 import Dialog from "./Dialog.js";
@@ -8,22 +9,32 @@ const allTodos = new Todos();
 const dialog = new Dialog();
 const audio = new Audio(pop);
 
+//Add non project todos;
 document.querySelector("#form").addEventListener("submit", (e) => {
   e.preventDefault();
   allTodos.add(createTodoFromForm());
   renderTodo(allTodos.todosArray);
   dialog.close();
+  snackbar.duration = 3000;
+  snackbar.show("Todo successfully added!");
 });
 
-document.addEventListener("change", (e) => {
-  if (e.target.matches(`input[type="checkbox"]`)) {
-    audio.play();
-  }
-});
-
+//expand todo info
 document.addEventListener("click", (e) => {
   if (e.target.classList.contains("expand_todo")) {
     const parent = e.target.parentElement;
     parent.nextElementSibling.classList.toggle("hide");
+  }
+});
+
+//remove Todo
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("todo_remove")) {
+    const parent = e.target.parentElement;
+    const closestTodo = e.target.closest(".todo");
+    const todoFullString = parent.previousElementSibling.textContent.trim();
+    closestTodo.remove();
+    allTodos.removeTodo(todoFullString);
+    audio.play();
   }
 });
